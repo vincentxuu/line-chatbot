21294d99d30d9ca5637a61e9226a5c726ddad7b3
vincentxu777 <vincentxu@pershing.com.tw>
2023-09-06 16:50:05 +0800

    fix

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', apiRouter);
 
 
