e6d324c67dda5a4c7d2049fba1bd5d4e14081a85
vincentxu777 <vincentxu@pershing.com.tw>
2023-09-06 11:47:40 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-// 建立express伺服器
-const express = require("express");
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+require('dotenv').config();
+
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
 
-var linebot = require('linebot');
-
-// 用於辨識Line Channel的資訊
-var bot = linebot({
-  channelId: process.env.channelId,
-  channelSecret: process.env.channelSecret,
-  channelAccessToken: process.env.channelAccessToken
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
-// 當有人傳送訊息給Bot時
-bot.on('message', function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-  var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
 
-  event.reply(replyMsg).then(function (data) {
-    // 當訊息成功回傳後的處理
-  }).catch(function (error) {
-    // 當訊息回傳失敗後的處理
-  });
-});
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
 
-// Bot所監聽的webhook路徑與port
-bot.listen('/linewebhook', 3000, function () {
-    console.log('[BOT已準備就緒]');
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
