4a8163bddadf6c7b5c7a4630975bb5abf2021ff4
vincentxu777 <vincentxu@pershing.com.tw>
2023-09-08 09:48:59 +0800

    update

--- controllers/watsonController.js
+++ controllers/watsonController.js
@@ -2,7 +2,8 @@ const AssistantV1 = require('ibm-watson/assistant/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 const getReply = async function (text){
-  let res
+  let res;
+  let conversationContext
   const assistant = new AssistantV1({
     version: '2021-06-14',
     authenticator: new IamAuthenticator({
@@ -11,9 +12,10 @@ const getReply = async function (text){
     serviceUrl: 'https://api.au-syd.assistant.watson.cloud.ibm.com',
   });
   try{
-  res = await assistant.message({
-    workspaceId: 'e5f2d935-02b9-4fd2-a801-eab8a3d45941',
-    input: text
+    res = await assistant.message({
+      workspaceId: 'e5f2d935-02b9-4fd2-a801-eab8a3d45941',
+      input: text,
+      context: this.conversationContext,
     });
   }catch(err){
       console.log(err);
@@ -22,13 +24,15 @@ const getReply = async function (text){
   console.log('getReply-.result.output',res.result.output);
   console.log('getReply-.result.output.generic',res.result.output.generic);
   console.log('getReply-.result.output.text',res.result.output.text);
-
   console.log('getReply-res.result.context',res.result.context);
 
 
-  if(res.result.output.text[0]){ 
-  console.log(res.result.output.text);
-  return res.result.output.text[0];
+  if(res?.status === 200){ 
+    this.conversationContext = res?.result?.context;
+    console.log(res.result.output.text);
+    return res.result.output.text[0];
+  }else {
+    return "可能在忙，晚點再試一次";
   }
 
 }
